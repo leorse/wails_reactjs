1fac51ecd2d7305fc758fc6546271c6bcfdd765b
leorse <damien.messner@gmail.com>
2022-06-12 15:34:43 +0200

    avancée dans le tuto pluralsight

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,22 +5,44 @@ import ph75 from './assets/images/75.png'
 //import App from './App'
 import MyCompo from './myCompo'
 
-interface IState {
-    title: string;
+interface infoCarz {
+    name: string,
+    avatar_url: string,
+    company:string
 }
 
-class Card extends React.Component {
+const testData:infoCarz[] = [
+    { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook0" },
+    { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+    { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+
+const CardList = (props: any) => (
+    <div>
+        <Card {...testData[0]}/>
+        <Card {...testData[1]}/>
+        <Card {...testData[2]}/>
+    </div>
+);
+
+class Card extends React.Component<infoCarz> {
     render() {
+        const profile = this.props;
         return (
-            <div className='github-profile'>
-                <img src={ph75} />
-                <div className="info">
-                    <div className="name">Name here...</div>
-                    <div className="company">Company here...</div>
+            <div className='github-profile' style={{ margin: '1rem' }}>
+                <img src={profile.avatar_url} />
+                <div className="info" style={{ display: 'inline-block', marginLeft: 10 }}>
+                    <div className="name" style={{ fontSize: '125%' }}>{profile.name}</div>
+                    <div className="company">{profile.company}</div>
                 </div>
             </div>
         );
     }
+}
+
+interface IState {
+    title: string;
 }
 
 class App extends React.Component<IState> {
@@ -28,13 +50,13 @@ class App extends React.Component<IState> {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <Card />
+                <CardList />
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App title="From github" />,
+    <App title="From github pouet" />,
     document.getElementById('root')
 );
